216ef3e9df2ad243f6fcdbcc1c60badc481f9187
Deoh <darylhyde_13@hotmail.com>
2020-11-13 20:29:00 +0000

    Finished product size logic in context.py

--- cart/contexts.py
+++ cart/contexts.py
@@ -15,14 +15,27 @@ def cart_contents(request):
 
     """Add to cart data"""
     for item_id, item_data in cart.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += item_data * product.price
-        product_count += item_data
-        cart_items.append({
-            'item_id': item_id,
-            'quantity': item_data,
-            'product': product,
-        })
+        # check to see if item has size by checking if item data is an integer
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     """
     free delivery if spending is more than the amount
