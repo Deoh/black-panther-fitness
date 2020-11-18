aedf495f4daab9131a40f815613186f176d10fab
Deoh <darylhyde_13@hotmail.com>
2020-11-18 20:04:09 +0000

    Added workout_class models

--- workout_class/admin.py
+++ workout_class/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Equipment, DifficultyLevel, WorkoutClass
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Equipment)
+admin.site.register(DifficultyLevel)
+admin.site.register(WorkoutClass)
--- workout_class/models.py
+++ workout_class/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from instructors.models import Instructor
 
 # Create your models here.
+
+
+class Category(models.Model):
+
+    class Meta:  # fix 'categorys' name in admin to 'categories'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class DifficultyLevel(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class WorkoutClass(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    duration = models.DurationField()
+    instructor = models.ForeignKey(
+        Instructor, null=True, blank=True, on_delete=models.SET_NULL)
+    class_size = models.PositiveSmallIntegerField()
+    equipment = models.ForeignKey(
+        'Equipment', null=True, blank=True, on_delete=models.SET_NULL)
+    difficulty_level = models.ForeignKey(
+        'DifficultyLevel', null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
